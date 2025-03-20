063a155edf7cf07dae62e7a47af92e30973d918e
FestusMike <festusmike98@gmail.com>
2025-03-20 17:15:45 +0100

    Docker configuration integrated

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -7,4 +7,4 @@ echo "Running migrations..."
 python manage.py migrate
 
 echo "Starting Gunicorn..."
-exec gunicorn -b 0.0.0.0:8000 controller.wsgi:application
+exec gunicorn -b 0.0.0.0:8000 controller.wsgi:application --reload
