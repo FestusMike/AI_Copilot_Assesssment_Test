325f31b8cedc79aacff12cfadb595e4bf093cab1
FestusMike <festusmike98@gmail.com>
2025-03-20 12:49:50 +0100

    ...

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exit script on any error
+set -e
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Starting Gunicorn..."
+exec gunicorn -b 0.0.0.0:8000 controller.wsgi:application
